bdff816f5954a81ba5eee7a3a43bc18d02320b55
Muhammad Zahid Masruri <masruri03@gmail.com>
2021-03-11 22:11:33 +0700

    initial commit

---
+++ test/plotter_test.py
@@ -0,0 +1,11 @@
+import unittest
+import numpy as np
+
+import sys
+sys.path.insert(1, 'D:/Documents/Kuliah/Project Akhir/Progress3/GUI')
+
+
+class TestPlotterMethods(unittest.TestCase):
+    def setUp(self):
+        from plotter import Plotter
+        self.plt = Plotter
