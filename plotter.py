75c075430716ae224c5fffe475a8454a5d71bcf3
Muhammad Zahid Masruri <masruri03@gmail.com>
2021-07-29 09:10:32 +0700

    change fft unit to dB

--- plotter.py
+++ plotter.py
@@ -32,16 +32,46 @@ class Plotter:
 
         self.axs[1, 0].clear()
         self.axs[1, 1].clear()
-        self.axs[1, 0].semilogy(freqs1, ps1)
-        self.axs[1, 1].semilogy(freqs2, ps2)
+        self.axs[1, 0].plot(freqs1, ps1)
+        self.axs[1, 1].plot(freqs2, ps2)
 
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
 
-    def doFFT(self, data):
-        time_step = 1/1000
-        ps = np.abs(np.fft.rfft(data))**2
-        freqs = np.fft.rfftfreq(data.size, d=time_step)
-        idx = np.argsort(freqs)
+    def doFFT(self, x, fs=1000, win=None, ref=1):
+        """
+        Calculate spectrum in dB scale
+        Args:
+            x: input signal
+            fs: sampling frequency
+            win: vector containing window samples (same length as x).
+                If not provided, then rectangular window is used by default.
+            ref: reference value used for dBFS scale. 32768 for int16 and 1 for float
+        Returns:
+            freq: frequency vector
+            s_db: spectrum in dB scale
+        """
+        N = len(x)  # Length of input sequence
+        if win is None:
+            win = np.ones(N)
+        if len(x) != len(win):
+            print('Window Lenght = ', len(win), 'Signal Length = ', len(x))
+            raise ValueError('Signal and window must be of the same length')
+        x = x * win
+        # Calculate real FFT and frequency vector
+        sp = np.fft.rfft(x)
+        freq = np.arange((N / 2) + 1) / (float(N) / fs)
 
-        return freqs[idx], ps[idx]
+        # Scale the magnitude of FFT by window and factor of 2,
+        # because we are using half of FFT spectrum.
+        s_mag = np.abs(sp) * 2 / np.sum(win)
+
+        # Convert to dBFS
+        s_dbfs = 20 * np.log10(s_mag/ref)
+
+        if len(freq) > len(s_dbfs):
+            freq = freq[:len(s_dbfs)]
+        if len(s_dbfs) > len(freq):
+            s_dbfs = s_dbfs[:len(freq)]
+
+        return freq, s_dbfs
